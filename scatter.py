f3c433812117b2171cfa771bdd8c9ffe46a4809e
pltaylor16 <peterllewelyntaylor@gmail.com>
2023-07-12 14:44:02 -0400

    starting to debug. Can now at least import module.

--- scatter.py
+++ scatter.py
@@ -247,64 +247,64 @@ class Scatter2D():
 
 
         for n1 in range(len(psi)):
-                j1 = psi[n1]['j']
-                theta1 = psi[n1]['theta']
+            j1 = psi[n1]['j']
+            theta1 = psi[n1]['theta']
 
-                U_1_c = self.cdgmm(U_0_c, psi[n1]['levels'][0])
-                if j1 > 0:
-                    U_1_c = self.subsample_fourier(U_1_c, k=2 ** j1)
-                U_1_c = self.ifft(U_1_c)
-                U_1_c = self.modulus(U_1_c)
-                U_1_c = self.rfft(U_1_c)
+            U_1_c = self.cdgmm(U_0_c, psi[n1]['levels'][0])
+            if j1 > 0:
+                U_1_c = self.subsample_fourier(U_1_c, k=2 ** j1)
+            U_1_c = self.ifft(U_1_c)
+            U_1_c = self.modulus(U_1_c)
+            U_1_c = self.rfft(U_1_c)
 
-                # Second low pass filter
-                S_1_c = self.cdgmm(U_1_c, phi['levels'][j1])
-                S_1_c = self.subsample_fourier(S_1_c, k=2 ** (self.J - j1))
+            # Second low pass filter
+            S_1_c = self.cdgmm(U_1_c, phi['levels'][j1])
+            S_1_c = self.subsample_fourier(S_1_c, k=2 ** (self.J - j1))
 
-                S_1_r = self.irfft(S_1_c)
-                S_1_r = self.unpad(S_1_r)
+            S_1_r = self.irfft(S_1_c)
+            S_1_r = self.unpad(S_1_r)
 
-                out_S_1.append({'coef': S_1_r,
-                                'j': (j1,),
-                                'n': (n1,),
-                                'theta': (theta1,)})
+            out_S_1.append({'coef': S_1_r,
+                            'j': (j1,),
+                            'n': (n1,),
+                            'theta': (theta1,)})
 
-                if max_order < 2:
+            if max_order < 2:
+                continue
+            for n2 in range(len(psi)):
+                j2 = psi[n2]['j']
+                theta2 = psi[n2]['theta']
+
+                if j2 <= j1:
                     continue
-                for n2 in range(len(psi)):
-                    j2 = psi[n2]['j']
-                    theta2 = psi[n2]['theta']
 
-                    if j2 <= j1:
-                        continue
+                U_2_c = self.cdgmm(U_1_c, psi[n2]['levels'][j1])
+                U_2_c = self.subsample_fourier(U_2_c, k=2 ** (j2 - j1))
+                U_2_c = self.ifft(U_2_c)
+                U_2_c = self.modulus(U_2_c)
+                U_2_c = self.rfft(U_2_c)
 
-                    U_2_c = self.cdgmm(U_1_c, psi[n2]['levels'][j1])
-                    U_2_c = self.subsample_fourier(U_2_c, k=2 ** (j2 - j1))
-                    U_2_c = self.ifft(U_2_c)
-                    U_2_c = self.modulus(U_2_c)
-                    U_2_c = self.rfft(U_2_c)
+                # Third low pass filter
+                S_2_c = self.cdgmm(U_2_c, phi['levels'][j2])
+                S_2_c = self.subsample_fourier(S_2_c, k=2 ** (self.J - j2))
 
-                    # Third low pass filter
-                    S_2_c = self.cdgmm(U_2_c, phi['levels'][j2])
-                    S_2_c = self.subsample_fourier(S_2_c, k=2 ** (self.J - j2))
+                S_2_r = self.irfft(S_2_c)
+                S_2_r = self.unpad(S_2_r)
 
-                    S_2_r = self.irfft(S_2_c)
-                    S_2_r = self.unpad(S_2_r)
+                out_S_2.append({'coef': S_2_r,
+                                'j': (j1, j2),
+                                'n': (n1, n2),
+                                'theta': (theta1, theta2)})
 
-                    out_S_2.append({'coef': S_2_r,
-                                    'j': (j1, j2),
-                                    'n': (n1, n2),
-                                    'theta': (theta1, theta2)})
+        out_S = []
+        out_S.extend(out_S_0)
+        out_S.extend(out_S_1)
+        out_S.extend(out_S_2)
 
-            out_S = []
-            out_S.extend(out_S_0)
-            out_S.extend(out_S_1)
-            out_S.extend(out_S_2)
+        if out_type == 'array':
+            out_S = stack([x['coef'] for x in out_S])
 
-            if out_type == 'array':
-                out_S = stack([x['coef'] for x in out_S])
-
-            return out_S
+        return out_S
 
     #deprecated
     '''
