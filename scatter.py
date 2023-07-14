79efc0118fa39c2a27da0f8e7568be4717e1f1a5
pltaylor16 <peterllewelyntaylor@gmail.com>
2023-07-14 16:07:07 -0400

    ready to make an attempt at the 3d code

--- scatter.py
+++ scatter.py
@@ -20,106 +20,13 @@ class Scatter2D():
             print ('ERROR: 2^J should be greater than M!')
 
 
-    #deprecated
-    '''
-    def get_js(self):
-        mx = int(log2(self.M))
-        self.js = list(range(mx+1))
-
-
-    def get_thetas(self):
-        self.thetas = []
-        for l in range(self.L):
-            self.thetas += [l/self.L * pi]
-    '''
 
 
     ###################################
     ### Wavelet functions #############
     ###################################
 
-    '''
-    def gabor_2d(self, xi, j, theta):
-
-
-        #rotations
-        R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]], np.float64)
-        R_inv = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]], np.float64)
-
-
-        #covariances
-        sigma = 0.8 * 2. ** j
-        Sigma_base = np.array([[1., 0], [0, self.slant ** 2]]) * 1. / (sigma ** 2.) 
-        Sigma = np.dot(R, np.dot(Sigma_base, R_inv))
-        #Sigma_inv = np.linalg.inv(Sigma)
-
-
-        #k_0_vector
-        k_0= xi * np.dot(R,np.array([0,1]))
-
-        #normalising factor
-        #norm = np.linalg.det(Sigma)
-
-        #gabor on the grid
-        gab = np.zeros((self.M, self.M), dtype = np.complex64)
-        k_0 =  k_0 / (2. * pi)
-        for kx in range(self.M):
-            for ky in range(self.M):
-                k = np.array([kx,ky]) / (2. * pi)
-                exponent = - np.dot((k - k_0).T, np.dot(Sigma,(k-k_0))) / 2. 
-                gab[kx,ky] = np.exp(exponent)
-
-        #gab *= norm ** -0.5
    
-
-        return gab 
-        '''
-
-    '''
-    def gabor_2d(self, xi, j, theta):
-
-        #rotations
-        R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]], np.float64)
-        R_inv = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]], np.float64)
-
-        #covariances
-        sigma = 0.8 * 2. ** j
-        Sigma_base = np.array([[1., 0], [0, self.slant ** 2]]) * sigma ** 2. 
-        Sigma = np.dot(R, np.dot(Sigma_base, R_inv))
-        Sigma_inv = np.linalg.inv(Sigma)
-
-        #k_0_vector
-        k_0= xi * np.dot(R,np.array([0,1]))
-
-
-        #normalising factor
-        norm = np.linalg.det(Sigma) ** 0.5 
-
-        n = self.M
-        #x = np.linspace(-n/2, n/2, self.M)
-        x = np.linspace(-n/2, n/2, self.M)
-
-        y = x.copy()
-        
-
-        gab = np.zeros((self.M, self.M), dtype = np.complex64)
-        for i in range(self.M):
-            for j in range(self.M):
-                x_vec = np.array([x[i],y[j]])
-                exponent = -np.dot(x_vec.T, np.dot(Sigma_inv,x_vec)) / 2.
-                exp1 = np.exp(exponent)
-      
-                exp2 = np.exp(1.j * np.dot(k_0, x_vec))
-
-
-                gab[i,j] = exp1 * exp2
-
-        return 1./norm * gab
-    '''
-
-
-   
-    
 
     
     def gabor_2d(self, xi, j, theta):
@@ -163,6 +70,7 @@ class Scatter2D():
         D = np.array([[1, 0], [0, slant * slant]])
         curv = np.dot(R, np.dot(D, R_inv)) / ( 2 * sigma * sigma)
 
+        # xx just is all the x valus in the grip ends up running from 64 to 95 for M = 32 for example
         for ex in [-2, -1, 0, 1, 2]:
             for ey in [-2, -1, 0, 1, 2]:
                 [xx, yy] = np.mgrid[offset + ex * M:offset + M + ex * M, offset + ey * N:offset + N + ey * N]
@@ -301,17 +209,6 @@ class Scatter2D():
         return x[..., 1:-1, 1:-1]
 
 
-    #utility function
-
-    ''' 
-    #this numpy version doesn't work
-    def pad(self, x, pad_size):
-        #adopted from numpy backend
-        paddings = ((0, 0),)      
-        paddings += ((pad_size[0], pad_size[1]), (pad_size[2], \
-            pad_size[3]))
-        return tf.pad(x, paddings, mode ='reflect')
-    '''
 
     #utility function
     def pad(self, x, pad_size, input_size):
@@ -386,15 +283,9 @@ class Scatter2D():
 
         #back to code
         U_r = self.pad(x, pad_size, input_size)
-        #print ('U_r:', np.shape(U_r))
 
         U_0_c = self.rfft(U_r)
 
-        #print ('U_0_c:', np.shape(U_r))
-        #print ('phi:', np.shape(phi['levels'][0]))
-
-        #print ('U_0_c:', U_0_c)
-        
 
         #first low pass filter
         U_1_C = self.cdgmm(U_0_c, phi['levels'][0])
@@ -433,7 +324,6 @@ class Scatter2D():
                             'j': (j1,),
                             'n': (n1,),
                             'theta': (theta1,)})
-            #print ('out_S_1:', out_S_1) #passed
 
 
             if max_order < 2:
@@ -475,187 +365,18 @@ class Scatter2D():
 
         return out_S
 
-    #deprecated
-    '''
-    def generate_filters(self):
-        filter_dict = {}
-        self.get_js()
-        self.get_thetas()
-
-
-        for j in self.js:
-            #hard-code this for now
-            xi_j = 3. * pi / 4. / 2. ** j
-            for l in range(self.L):
-
-                filter_dict['psi,j:%s,l:%s' %(j,l)] = self.morlet_2d(xi_j, j, l)
-                filter_dict['phi,j:%s,l:%s' %(j,l)] = self.gabor_2d(0., self.J-1, l)
-
-        return filter_dict
-
-
-    def get_filters_tensor(self):
-        
-        filters_np_phi = np.zeros((self.J, self.L, self.M, self.M), dtype=np.complex64)
-        filters_np_psi = np.zeros((self.J, self.L, self.M, self.M), dtype=np.complex64)
-        for j in range(self.J):
-            for l in range(self.L):
-                filters_np_phi[j,l,:,:] = self.generate_filters()['phi,j:%s,l:%s' %(j,l)]
-                filters_np_psi[j,l,:,:] = self.generate_filters()['psi,j:%s,l:%s' %(j,l)]
-
-
-        phi = tf.convert_to_tensor(filters_np_phi, dtype= tf.complex64)
-        psi = tf.convert_to_tensor(filters_np_psi, dtype= tf.complex64)
-
-        
-        return phi, psi
-    '''
-
-
-
     
-    '''
-    def compute_coefs(self, data_batch, filters):
-
-        #filter has dimesnions (J,L,M,M), data_batch has dimesnions \
-        (batch_size,M,M)
-
-
-        #convert input to complex tensor
-        dat = tf.convert_to_tensor(data_batch, dtype= tf.complex64)
-        batch_size = np.shape(data_batch)[0]
-        del data_batch
-
-        #Fourier transform
-        dat_f = tf.signal.fft2d(dat) 
-
-        #compute 
-        M1 = tf.zeros((batch_size, self.J, self.L, self.M, self.M), dtype = tf.complex64)
-        S1 = tf.zeros((batch_size, self.J, self.L), dtype = tf.complex64)
-
-        M1 = tf.signal.ifft2d(dat_f[:,None,None,:,:] * filters[None,:,:,:,:])
-        S1 = tf.math.reduce_mean(M1, axis = (3,4))
-
-        return S1
-    '''
-    
-
-
-
-
-
 
 class Scatter3D():
     
-    def __init__(self, J, L, M, slant1, slant2, max_order = 2):
+    def __init__(self, J, L, M, slant1, slant2):
 
         self.J = J
         self.L = L 
         self.M = M 
         self.slant1 = slant1
-        self.slant2 = slant2
-        self.max_order = max_order
+        self.slant2 =slant2
 
-        if 2 ** self.J > self.M:
-            print ('ERROR: 2^J should be greater than M!')
-
-
-    def get_js(self):
-        mx = int(log2(self.M))
-        self.js = list(range(mx+1))
-
-
-    def get_thetas(self):
-        self.thetas = []
-        for l in range(self.L):
-            self.thetas += [l/self.L * pi]
-
-    def get_phis(self):
-        self.phis = []
-        for l in range(self.L):
-            self.phis += [l/self.L * pi]
-
-
-
-    def gabor_3d(self, xi, j, l_theta, l_phi):
-
-        theta = self.thetas[l_theta]
-        phi = self.phis[l_phi]
-
-
-        #rotations
-        R_1 = np.array([[np.cos(theta), -np.sin(theta), 0.],\
-            [np.sin(theta), np.cos(theta), 0.], [0., 0., 1.]], np.float64)
-        R_2 = np.array([[np.cos(phi), 0., -np.sin(phi)], \
-            [0., 1., 0.], [np.sin(phi), 0., np.cos(phi)]], np.float64)
-        R = np.dot(R_1, R_2)
-        R_T = R.T
-
-        #covariances
-        sigma = 0.8 * 2. ** j
-        Sigma_base = np.array([[sigma ** 2, 0, 0], [0, sigma ** 2 / self.slant1 ** 2, 0], [0, 0, sigma ** 2 / self.slant2 ** 2]])
-        Sigma = np.dot(R, np.dot(Sigma_base, R_T))
-        #Sigma_inv = np.linalg.inv(Sigma)
-
-
-        #k_0_vector
-        k_0= xi * np.dot(R,np.array([1,0,0]))
-
-        #normalising factor
-        norm = np.linalg.det(Sigma)
-
-        #gabor on the grid
-        gab = np.zeros((self.M, self.M, self.M), dtype = np.complex64)
-        k_0 =  k_0 / (2 * pi)
-        for kx in range(self.M):
-            for ky in range(self.M):
-                for kz in range(self.M):
-                    k = np.array([kx,ky,kz]) / (2. * pi)
-                    exponent = - np.dot((k - k_0).T, np.dot(Sigma,(k-k_0))) / 2.
-                    gab[kx,ky,kz] = np.exp(exponent)
-   
-
-        return gab 
-
-
-
-    def morlet_3d(self, xi, j, l_theta, l_phi):
-
-        
-        val = self.gabor_3d(xi, j, l_theta, l_phi)
-        modulus = self.gabor_3d(0., j, l_theta, l_phi)
-        K = np.sum(val) / np.sum(modulus)
-  
-
-        mor = val - K * modulus
-
-        return mor 
-
-
-    def generate_filters(self):
-        filter_dict = {}
-        self.get_js()
-        self.get_thetas()
-        self.get_phis()
-
-
-        for j in self.js:
-            #hard-code this for now
-            xi_j = 3. * pi / 4. / 2. ** j
-            for l_theta in range(self.L):
-                for l_phi in range(self.L):
-
-
-                    filter_dict['psi,j:%s,l_theta:%s,l_phi:%s' %(j,l_theta,l_phi)] = self.morlet_3d(xi_j, j, l_theta, l_phi)
-                    filter_dict['phi,j:%s,l_theta:%s,l_phi:%s' %(j,l_theta,l_phi)]  = self.gabor_2d(0., self.J-1, l_theta, l_phi)
-
-        return filter_dict
-
-
-
-
-    def compute_coeffecient(self):
-        pass
 
 
 
